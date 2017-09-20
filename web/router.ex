33eaa26fce5ccfbafb5419bf262de7cb0e395f23
Hossein Ahmadian-Yazdi <hyazdi1997@gmail.com>
2017-09-20 08:26:39 -0400

    Lots of updates

--- web/router.ex
+++ web/router.ex
@@ -16,8 +16,7 @@ defmodule Discuss.Router do
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
-    get "/topics/new", TopicController, :new
+    resources "/", TopicController
   end
 
   # Other scopes may use custom stacks.
